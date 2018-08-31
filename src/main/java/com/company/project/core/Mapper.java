523178360774e665bcdbb6e59ebd1550dabe630b
iblilife@Aukey-PC <iblilife@163.com>
2018-08-31 11:32:48 +0800

    Initial commit

---
+++ src/main/java/com/company/project/core/Mapper.java
@@ -0,0 +1,22 @@
+package com.company.project.core;
+
+/**
+ * @Description:
+ * @Author: iblilife@163.com
+ * @Date: 10:09 2018/8/31
+ */
+import tk.mybatis.mapper.common.BaseMapper;
+import tk.mybatis.mapper.common.ConditionMapper;
+import tk.mybatis.mapper.common.IdsMapper;
+import tk.mybatis.mapper.common.special.InsertListMapper;
+
+/**
+ * 定制版MyBatis Mapper插件接口，如需其他接口参考官方文档自行添加。
+ */
+public interface Mapper<T>
+        extends
+        BaseMapper<T>,
+        ConditionMapper<T>,
+        IdsMapper<T>,
+        InsertListMapper<T> {
+}
